1321285c67052ba2a7f74eadec41f566a8f9f22a
Luis Martinez <luismartinez@palo-it.com>
2021-03-13 17:01:28 -0500

    Updating the data object to use a map with directions to store the values per each cell

--- src/main/java/com/meli/challenge/dto/DNACharSequence.java
+++ src/main/java/com/meli/challenge/dto/DNACharSequence.java
@@ -1,22 +1,28 @@
 package com.meli.challenge.dto;
 
+import com.meli.challenge.enums.Direction;
 import lombok.Builder;
 import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Data
 @Builder
 public class DNACharSequence {
-    String base;
-    int up;
-    int upRight;
-    int right;
-    int downRight;
-    int down;
-    int downLeft;
-    int left;
-    int upLeft;
+    Character base;
+    Map<Direction, Integer> directions;
 
-    public boolean isSameBase(String base){
-        return this.base != null && this.base.equalsIgnoreCase(base);
+    public boolean isSameBase(char base){
+        return this.base == base;
+    }
+
+    public void setDirection(Direction direction, Integer value) {
+        if (directions == null) directions = new HashMap<>();
+        directions.put(direction, value);
+    }
+
+    public Integer getDirectionValue(Direction direction) {
+        return directions.getOrDefault(direction, 0);
     }
 }
---
+++ src/main/java/com/meli/challenge/enums/Direction.java
@@ -0,0 +1,12 @@
+package com.meli.challenge.enums;
+
+public enum Direction {
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT,
+    UP_RIGHT,
+    DOWN_RIGHT,
+    DOWN_LEFT,
+    UP_LEFT
+}
